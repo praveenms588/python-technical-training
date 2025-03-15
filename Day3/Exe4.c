7524d363fb317835212eb05c3c8c2780f6d1e41c
Praveen M S <praveenms588@gmail.com>
2025-03-15 09:37:46 +0530

    Update Exe4.c

--- Day3/Exe4.c
+++ Day3/Exe4.c
@@ -1,17 +1,16 @@
+def check_key(dictionary, key):
+    if find_key in student_details:
+        print(f"{find_key}: was found")
+    else:
+        print(f"{find_key}: was not found")
+
 student_details={
     "name":"Praveen",
     "id": 121,
     "branch":"ECE",
-    "year":2026
+    "year":202
     
 }
-name=student_details["name"];
-id=student_details["id"];
-branch=student_details["branch"];
-year=student_details["year"];
 
-student_details["blood_group"]="A+ve"
 find_key=input("Enter a key to search:")
-if find_key in student_details:
-    print(f"{find_key}: was found")
-print(f"{find_key}: was not found")
+check_key(student_details, find_key);
