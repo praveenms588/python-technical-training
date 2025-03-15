6957d762ad550c026dbf0bfe9276ca8335b4de08
Praveen M S <praveenms588@gmail.com>
2025-03-15 09:17:48 +0530

    Create Exe1.c

---
+++ Day3/Exe1.c
@@ -0,0 +1,14 @@
+student_details={
+    "name":"Praveen",
+    "id": 121,
+    "branch":"ECE",
+    "year":2026
+    
+}
+name=student_details["name"];
+id=student_details["id"];
+branch=student_details["branch"];
+year=student_details["year"];
+
+
+print(student_details)
