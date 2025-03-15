d3b73d681846c82f3d9ce62a463215a3d12ddf90
Praveen M S <praveenms588@gmail.com>
2025-03-15 09:19:06 +0530

    Update Exe2.c

--- Day3/Exe2.c
+++ Day3/Exe2.c
@@ -9,6 +9,6 @@ name=student_details["name"];
 id=student_details["id"];
 branch=student_details["branch"];
 year=student_details["year"];
-
+del student_details["id"]
 student_details["blood_group"]="A+ve"
 print(student_details)
