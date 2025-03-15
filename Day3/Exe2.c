27c12b4c5e2c4d3749110ebe6fb5321613c63717
Praveen M S <praveenms588@gmail.com>
2025-03-15 09:18:11 +0530

    Create Exe2.c

---
+++ Day3/Exe2.c
@@ -0,0 +1,14 @@
+student_details={
+    "name":"Praveen",
+    "id": 121,
+    "branch":"ECE",
+    "year":2026
+    
+}
+name=student_details["name"];
+id=student_details["id"];
+branch=student_details["branch"];
+year=student_details["year"];
+
+student_details["blood_group"]="A+ve"
+print(student_details)
