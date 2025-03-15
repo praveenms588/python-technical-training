f5562e020d6a519af35af2dea7e625a388fa8e8a
Praveen M S <praveenms588@gmail.com>
2025-03-15 09:23:58 +0530

    Create Exe3.c

---
+++ Day3/Exe3.c
@@ -0,0 +1,15 @@
+student_details={
+    "name":"Praveen",
+    "id": 121,
+    "branch":"ECE",
+    "year":2026
+    
+}
+name=student_details["name"];
+id=student_details["id"];
+branch=student_details["branch"];
+year=student_details["year"];
+
+student_details["blood_group"]="A+ve"
+for key, value in student_details.items():
+    print(f"{key}: {value}")
